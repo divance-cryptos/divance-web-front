ff9a72cd6650cc03be5884d457f3fe20b115aec6
Josep Servat <jservat@outlook.es>
2021-09-11 00:52:20 +0200

    Add eslint

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import { Link, Route } from "wouter";
+import { Route } from "wouter";
 import Home from "../src/pages/Home";
 
 const App = () => {
   return (
     <div className="App">
-        <Route component={Home} path="/"></Route>
+      <Route component={Home} path="/"></Route>
     </div>
   );
-}
+};
 
 export default App;
