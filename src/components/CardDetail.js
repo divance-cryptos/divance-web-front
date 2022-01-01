0d410fa355dfc6f1ded79b9895184d43e19c2c58
Josep Servat <jservat@outlook.es>
2022-01-02 00:09:33 +0100

    fix require

--- src/components/CardDetail.js
+++ src/components/CardDetail.js
@@ -10,7 +10,7 @@ const ItemCover = styled.div`
     height: 220px;
     width: 220px;
 `;
-  
+
 const Image = styled.img`
     height: 220px;
     width: 220px;
@@ -20,7 +20,7 @@ const LiveDetail = ({ watchLink, thumb }) => {
   return (
     <Container>
       <ItemCover>
-        <a href={watchLink}><Image src={require(`../assets/${thumb}.png`).default} alt="" width="260px" /></a>
+        <a href={watchLink}><Image src={require(`../assets/${thumb}.png`)} alt="" width="260px" /></a>
       </ItemCover>
     </Container>
   );
